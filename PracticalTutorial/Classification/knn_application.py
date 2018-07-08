756b40118bb03a84728dd88ecfb181b38efb727a
Unknown <a.progerz@gmail.com>
2018-07-08 22:40:08 +0300

    Knn tutorials finished

--- PracticalTutorial/Classification/knn_application.py
+++ PracticalTutorial/Classification/knn_application.py
@@ -59,7 +59,7 @@ clf = neighbors.KNeighborsClassifier()
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+print('Accuracy:', accuracy)
 
 # EXAMPLE PREDICTION
 # example_measures = np.array([4,2,1,1,1,2,3,2,1])
