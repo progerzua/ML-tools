3d13d7de2bc12529bf0c2d6f7a151ab1bfec6401
Unknown <a.progerz@gmail.com>
2018-07-18 22:13:15 +0300

    SVM finished

---
+++ PracticalTutorial/SVM/svm_application.py
@@ -0,0 +1,23 @@
+'''
+Benign = class 2 = good
+Malignant = class 4 = bad
+'''
+
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors, svm
+import pandas as pd
+
+df = pd.read_csv('../Classification/breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y, test_size=0.2)
+
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print('Accuracy:', accuracy)
