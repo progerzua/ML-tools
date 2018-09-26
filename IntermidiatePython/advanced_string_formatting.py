356664dbeb85e9e77da3c71f85c1cbc1ea5f6ee4
Unknown <a.progerz@gmail.com>
2018-09-26 23:05:56 +0300

    PyTorch Intro passed

---
+++ IntermidiatePython/advanced_string_formatting.py
@@ -0,0 +1,17 @@
+# Based on this
+# https://www.youtube.com/watch?list=PLQVvvaa0QuDfju7ADVp5W1GF9jVhjbX-_&time_continue=6&v=jA5LW3bR0Us
+# Pretty simple one buuuuuut
+
+names = ['Jeff', 'Gary', 'Jill', 'Samantha']
+
+##for name in names:
+##    #print('Hello there, ' + name)
+##    print(' '.join(['Hello there', name]))
+
+#print(', '.join(names))
+
+who = 'Gary'
+how_many = 12
+
+print(who, 'bought', how_many)
+print('{} bought {} apples today'.format(who, how_many))
